458f56169d56ddda1138210555be1a047a91dc41
M0rt3-K <60618540+M0rt3-K@users.noreply.github.com>
2022-12-01 15:04:43 -0300

    Add files via upload

---
+++ att.java
@@ -0,0 +1,51 @@
+package MapaPDS2;
+
+public class att {
+
+    private String cpfc;
+    private String nomec;
+    private String d1c;
+    private String d2c;
+    private String d3c;
+    private String d4c;
+
+    public void atribuirt1(String cpfc) {
+        this.cpfc = cpfc;
+        
+    }
+    public String pegarcpfc() {
+        return cpfc;
+    }
+    public void atribuirt2(String nomec) {
+        this.nomec = nomec;
+        
+    }
+    public String pegarnomec() {
+        return nomec;
+    }
+    public void atribuirt3(String d1c) {
+        this.d1c = d1c;
+    }
+    public String pegard1c() {
+        return d1c;
+    }
+    public void atribuirt4(String d2c) {
+        this.d2c = d2c;
+    }
+    public String pegard2c() {
+        return d2c;
+    }
+    public void atribuirt5(String d3c) {
+        this.d3c = d3c;
+    }
+    public String pegard3c() {
+        return d3c;
+    }
+    public void atribuirt6(String d4c) {
+        this.d4c = d4c;
+    }
+    public String pegard4c() {
+        return d4c;
+    }
+
+}
